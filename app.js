94857815400c7813fbca081b20bb0bfeb8306322
dev-laksh <70800648+dev-laksh@users.noreply.github.com>
2020-10-06 08:00:25 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const charactersList = document.getElementById('charactersList');
+let hpCharacters = [];
+const searchBar = document.getElementById('searchBar');
+
+console.log(searchBar);
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+    const filteredCharacters = hpCharacters.filter(character => {
+        return (
+            character.name.toLowerCase().includes(searchString) || character.house.toLowerCase().includes(searchString)
+        );
+    });
+    displayCharacters(filteredCharacters);
+});
+const loadCharacters = async () => {
+    try {
+        const res = await fetch('https://hp-api.herokuapp.com/api/characters');
+        hpCharacters = await res.json();
+        displayCharacters(hpCharacters);
+        console.log(hpCharacters);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const displayCharacters = (characters) => {
+    const htmlString = characters
+        .map((character) => {
+            return `
+            <li class="character">
+                <h2>${character.name}</h2>
+                <p>House: ${character.house}</p>
+                <img src="${character.image}"></img>
+            </li>
+        `;
+        })
+        .join('');
+    charactersList.innerHTML = htmlString;
+};
+
+loadCharacters();
